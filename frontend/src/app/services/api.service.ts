60ff82bfa414cb6724fc6292f71dbee263ab5be9
Dazinha <pablo.daza.g@mail.pucv.cl>
2025-06-05 01:01:25 -0400

    feat: conectar API en categorias, especies, noticias y usuarios

--- frontend/src/app/services/api.service.ts
+++ frontend/src/app/services/api.service.ts
@@ -1,13 +1,28 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class UsuarioService {
+export class ApiService {
+  private baseUrl = 'http://localhost:8100';
+
   constructor(private http: HttpClient) {}
 
-  getUsuarios() {
-    return this.http.get('http://localhost:3000/api/usuarios');
+  getCategorias(): Observable<any> {
+    return this.http.get(`${this.baseUrl}/categorias`);
+  }
+  
+  getEspecies(): Observable<any> {
+    return this.http.get(`${this.baseUrl}/especies`);
+  }
+
+  getNoticias(): Observable<any> {
+    return this.http.get(`${this.baseUrl}/noticias`);
+  }
+
+  getUsuarios(): Observable<any> {
+    return this.http.get(`${this.baseUrl}/usuarios`);
   }
 }
